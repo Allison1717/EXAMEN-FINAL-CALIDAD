fcb84c154220b5ebaec1387ced5258e3700b11cb
Allison Andrea Rodriguez Sanchez <allisonandrea712@gmail.com>
2022-11-30 23:52:35 -0500

    EXAMEN FINAL DE CALIDAD

---
+++ Calidad20222.webTests/Services/PokerServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calidad20222.web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calidad20222.web.Services.Tests
+{
+    [TestClass()]
+    public class PokerServiceTests
+    {
+        [TestMethod()]
+        public void GetJugadaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
